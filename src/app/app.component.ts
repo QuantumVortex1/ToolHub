07409cc6274c7ae0a9cd1471176e8bff79b8a75c
QuantumVortex1 <152855101+QuantumVortex1@users.noreply.github.com>
2025-10-06 20:01:03 +0200

    feat: add footer with version and link to GitHub and ko-fi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header/header.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, SidenavComponent],
+  imports: [RouterOutlet, HeaderComponent, SidenavComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
