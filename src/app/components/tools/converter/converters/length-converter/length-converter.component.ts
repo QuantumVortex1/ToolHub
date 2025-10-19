bb1b26a5046aba6bf7c41caba9688e5816d14f0e
QuantumVortex1 <152855101+QuantumVortex1@users.noreply.github.com>
2025-10-19 16:22:32 +0200

    refactor: Simplify converter components by extending BaseConverterComponent

--- src/app/components/tools/converter/converters/length-converter/length-converter.component.ts
+++ src/app/components/tools/converter/converters/length-converter/length-converter.component.ts
@@ -1,18 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule, Location } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { LENGTH_CONVERTER } from './length.converter';
-import { UnitDefinition } from '../../converter-types';
 import { MatIconModule } from "@angular/material/icon";
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-
-interface UnitCard {
-  unit: UnitDefinition;
-  value: number | null;
-}
+import { BaseConverterComponent } from '../../services/base-converter.component';
 
 @Component({
   selector: 'app-length-converter',
@@ -20,103 +15,28 @@ interface UnitCard {
   templateUrl: './length-converter.component.html',
   styleUrl: './length-converter.component.css'
 })
-export class LengthConverterComponent implements OnInit {
-  converter = LENGTH_CONVERTER;
-  unitCards: UnitCard[] = [];
-  
-  private baseValue: number | null = null;
-  private lastEditedUnit: UnitDefinition | null = null;
+export class LengthConverterComponent extends BaseConverterComponent {
+  override converter = LENGTH_CONVERTER;
 
   constructor(
-    private route: ActivatedRoute,
-    private router: Router,
-    private location: Location
-  ) {}
-
-  ngOnInit() {
-    this.unitCards = this.converter.units.map(unit => ({
-      unit,
-      value: null
-    }));
-
-    this.route.queryParams.subscribe(params => {
-      const value = params['value'];
-      const unit = params['unit'] || 'm';
-      
-      if (value) {
-        const initialValue = parseFloat(value);
-        const initialUnit = this.converter.units.find(u => u.unit === unit);
-        
-        if (initialUnit && !isNaN(initialValue))  this.updateFromInput(initialUnit, initialValue);
-      }
-    });
+    route: ActivatedRoute,
+    router: Router,
+    location: Location
+  ) {
+    super(route, router, location);
+    this.defaultUnit = 'm';
   }
 
-  onInputChange(card: UnitCard) {
-    if (card.value !== null && card.value !== undefined && !isNaN(card.value)) {
-      this.updateFromInput(card.unit, card.value);
-      this.updateUrl(card.unit, card.value);
-    } else {
-      this.baseValue = null;
-      this.unitCards.forEach(c => {
-        if (c !== card) c.value = null;
-      });
-    }
+  get metricUnits() {
+    return this.filterUnitsByIds(['km', 'm', 'cm', 'mm']);
   }
 
-  private updateFromInput(sourceUnit: UnitDefinition, value: number) {
-    this.lastEditedUnit = sourceUnit;
-    this.baseValue = sourceUnit.toBase(value);
-    
-    this.unitCards.forEach(card => {
-      if (card.unit !== sourceUnit) {
-        let convertedValue = card.unit.fromBase(this.baseValue!);
-        card.value = this.roundToPrecision(convertedValue, 12);
-      } else {
-        card.value = value;
-      }
-    });
+  get imperialUnits() {
+    return this.filterUnitsByIds(['mi', 'yd', 'ft', 'in']);
   }
 
-  private roundToPrecision(value: number, precision: number): number {
-    if (value === 0) return 0;
-    const multiplier = Math.pow(10, precision - Math.floor(Math.log10(Math.abs(value))) - 1);
-    return Math.round(value * multiplier) / multiplier;
-  }
-
-  private updateUrl(unit: UnitDefinition, value: number) {
-    const urlTree = this.router.createUrlTree([], {
-      relativeTo: this.route,
-      queryParams: { 
-        value: value.toString(),
-        unit: unit.unit
-      },
-      queryParamsHandling: 'merge'
-    });
-
-    this.location.replaceState(urlTree.toString());
-  }
-
-  isHighlighted(card: UnitCard): boolean {
-    return this.lastEditedUnit === card.unit;
-  }
-
-  get metricUnits(): UnitCard[] {
-    return this.unitCards.filter(c => 
-      ['km', 'm', 'cm', 'mm'].includes(c.unit.unit)
-    );
-  }
-
-  get imperialUnits(): UnitCard[] {
-    return this.unitCards.filter(c => 
-      ['mi', 'yd', 'ft', 'in'].includes(c.unit.unit)
-    );
-  }
-
-  get otherUnits(): UnitCard[] {
-    return this.unitCards.filter(c => 
-      ['nmi', 'ly'].includes(c.unit.unit)
-    );
+  get otherUnits() {
+    return this.filterUnitsByIds(['nmi', 'ly']);
   }
 }
 
---
+++ src/app/components/tools/converter/services/base-converter.component.ts
@@ -0,0 +1,109 @@
+import { OnInit, Directive } from '@angular/core';
+import { Location } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ConverterDefinition, UnitDefinition } from '../converter-types';
+
+export interface UnitCard {
+  unit: UnitDefinition;
+  value: number | null;
+}
+
+@Directive()
+export abstract class BaseConverterComponent implements OnInit {
+  abstract converter: ConverterDefinition;
+  unitCards: UnitCard[] = [];
+
+  protected baseValue: number | null = null;
+  protected lastEditedUnit: UnitDefinition | null = null;
+  protected defaultUnit: string = '';
+  protected precision: number = 12;
+
+  constructor(
+    protected route: ActivatedRoute,
+    protected router: Router,
+    protected location: Location
+  ) {}
+
+  ngOnInit() {
+    this.initializeUnitCards();
+    this.subscribeToQueryParams();
+  }
+
+  protected initializeUnitCards(): void {
+    this.unitCards = this.converter.units.map(unit => ({
+      unit,
+      value: null
+    }));
+  }
+
+  protected subscribeToQueryParams(): void {
+    this.route.queryParams.subscribe(params => {
+      const value = params['value'];
+      const unit = params['unit'] || this.defaultUnit || this.converter.baseUnit;
+
+      if (value) {
+        const initialValue = parseFloat(value);
+        const initialUnit = this.converter.units.find(u => u.unit === unit);
+
+        if (initialUnit && !isNaN(initialValue)) this.updateFromInput(initialUnit, initialValue);
+      }
+    });
+  }
+
+  onInputChange(card: UnitCard): void {
+    if (card.value !== null && card.value !== undefined && !isNaN(card.value)) {
+      this.updateFromInput(card.unit, card.value);
+      this.updateUrl(card.unit, card.value);
+    } else {
+      this.clearAllValues(card);
+    }
+  }
+
+  protected clearAllValues(excludeCard?: UnitCard): void {
+    this.baseValue = null;
+    this.unitCards.forEach(c => {
+      if (c !== excludeCard) c.value = null;
+    });
+  }
+
+  protected updateFromInput(sourceUnit: UnitDefinition, value: number): void {
+    this.lastEditedUnit = sourceUnit;
+    this.baseValue = sourceUnit.toBase(value);
+
+    this.unitCards.forEach(card => {
+      if (card.unit !== sourceUnit) {
+        const convertedValue = card.unit.fromBase(this.baseValue!);
+        card.value = this.roundToPrecision(convertedValue, this.precision);
+      } else {
+        card.value = value;
+      }
+    });
+  }
+
+  protected roundToPrecision(value: number, precision: number): number {
+    if (value === 0) return 0;
+    const multiplier = Math.pow(10, precision - Math.floor(Math.log10(Math.abs(value))) - 1);
+    return Math.round(value * multiplier) / multiplier;
+  }
+
+  protected updateUrl(unit: UnitDefinition, value: number): void {
+    const urlTree = this.router.createUrlTree([], {
+      relativeTo: this.route,
+      queryParams: {
+        value: value.toString(),
+        unit: unit.unit
+      },
+      queryParamsHandling: 'merge'
+    });
+
+    this.location.replaceState(urlTree.toString());
+  }
+
+  isHighlighted(card: UnitCard): boolean {
+    return this.lastEditedUnit === card.unit;
+  }
+
+  protected filterUnitsByIds(unitIds: string[]): UnitCard[] {
+    return this.unitCards.filter(c => unitIds.includes(c.unit.unit));
+  }
+}
